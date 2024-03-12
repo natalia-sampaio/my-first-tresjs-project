611e5ddd7242a413850a4439fc02e4920eb326df
Natalia Andrade <natalia.srsa@proton.me>
2024-03-12 10:19:56 -0300

    creating geometries

--- src/components/TheExperience.vue
+++ src/components/TheExperience.vue
@@ -1,45 +1,19 @@
 <script setup lang="ts">
 import { TresCanvas } from '@tresjs/core';
-import { reactive, shallowRef, watch } from 'vue';
-import { BasicShadowMap, SRGBColorSpace, NoToneMapping, PerspectiveCamera } from 'three';
-import gsap from 'gsap';
+import { SRGBColorSpace } from 'three';
+import { OrbitControls } from '@tresjs/cientos';
 
-const state = reactive({
-  clearColor: '#2B3846',
-  shadows: true,
-  alpha: false,
-  shadowMapType: BasicShadowMap,
-  outputColorSpace: SRGBColorSpace,
-  toneMapping: NoToneMapping
-});
-
-const cameraRef = shallowRef<PerspectiveCamera>();
-
-watch(cameraRef, (camera) => {
-  if (camera) {
-    gsap.to(camera.position, {
-      delay: 1,
-      duration: 2,
-      x: 11,
-      y: 11,
-      z: 11,
-      ease: 'power3.out',
-      onUpdate: () => {
-        camera.lookAt(0, 0, 0);
-      }
-    });
-  }
-});
+const positionsArray = new Float32Array([0, 0, 0, 0, 1, 0, 1, 0, 0]);
 </script>
 
 <template>
-  <TresCanvas v-bind="state">
-    <TresPerspectiveCamera ref="cameraRef" :args="[45, 1, 0.1, 1000]" />
-    <TresMesh :position="[0, 0.5, 0]">
-      <TresBoxGeometry />
-      <TresMeshNormalMaterial />
+  <TresCanvas clear-color="#2c3846" :output-color-space="SRGBColorSpace">
+    <TresPerspectiveCamera />
+    <OrbitControls />
+    <TresMesh>
+      <TresBufferGeometry :position="[positionsArray, 3]" />
+      <TresMeshBasicMaterial color="#ff0000" wireframe />
     </TresMesh>
-
-    <TresGridHelper :args="[4]" />
+    <TresAxesHelper :args="[0.5]" />
   </TresCanvas>
 </template>

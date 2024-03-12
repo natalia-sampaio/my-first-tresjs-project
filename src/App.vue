f9b7b353341186ad9091ff8316b84ce16610b736
Natalia Andrade <natalia.srsa@proton.me>
2024-03-11 22:06:08 -0300

    first steps and animation

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,44 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { TresCanvas, useRenderLoop } from '@tresjs/core'
+import { shallowRef, watchEffect } from 'vue'
+
+const { onLoop } = useRenderLoop()
+
+const cubeRef = shallowRef()
+
+onLoop(({ delta, elapsed }) => {
+  if (cubeRef.value) {
+    cubeRef.value.rotation.y += delta
+    cubeRef.value.rotation.z = elapsed
+
+    cubeRef.value.position.y = Math.sin(elapsed * 2)
+    cubeRef.value.scale.set(Math.sin(elapsed * 2), Math.sin(elapsed * 2), Math.sin(elapsed * 2))
+  }
+})
+
+watchEffect(() => {
+  console.log({ cubeRef })
+})
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <TresCanvas>
+    <TresPerspectiveCamera />
+    <TresMesh ref="cubeRef" :position="[0, 0, 0]">
+      <TresBoxGeometry />
+      <TresMeshNormalMaterial />
+    </TresMesh>
+    <TresAxesHelper />
+  </TresCanvas>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
+<style>
+html,
+body,
+#app {
+  margin: 0;
+  padding: 0;
+  height: 100%;
   width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
 }
 </style>

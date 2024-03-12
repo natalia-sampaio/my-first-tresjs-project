bc8ef8775d5a9fffd51bef65729a89124a3ebc10
Natalia Andrade <natalia.srsa@proton.me>
2024-03-12 00:30:45 -0300

    trying v2 new stuff

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,27 @@
 <script setup lang="ts">
-import { TresCanvas, useRenderLoop } from '@tresjs/core';
-import { ref } from 'vue';
+import { TresCanvas } from '@tresjs/core';
 import { OrbitControls } from '@tresjs/cientos';
+import { reactive } from 'vue';
+import { BasicShadowMap, NoToneMapping } from 'three';
 
-const torusRef = ref();
-const torusPosition = ref([0, 0, 0]);
-
-const { onLoop } = useRenderLoop();
-
-onLoop(({ delta, elapsed }) => {
-  if (!torusRef.value) return;
-
-  torusRef.value.rotation.x += delta;
-  torusRef.value.rotation.y += delta;
+const state = reactive({
+  clearColor: '#82dbc5',
+  shadows: true,
+  alpha: false,
+  shadowMapType: BasicShadowMap,
+  toneMapping: NoToneMapping
 });
 </script>
 
 <template>
-  <TresCanvas window-size>
-    <TresPerspectiveCamera :args="[75, 1, 0.1, 1000]" :position="[3, 1, 4]" :look-at="[0, 0, 0]" />
+  <TresCanvas v-bind="state" window-size>
+    <TresPerspectiveCamera :position="[7, 7, 7]" :look-at="[0, 0, 0]" />
     <OrbitControls />
-    <TresScene>
-      <TresMesh ref="torusRef" :position="torusPosition">
-        <TresTorusKnotGeometry :args="[1, 0.4, 100, 16]" />
-        <TresMeshNormalMaterial />
-      </TresMesh>
-    </TresScene>
+    <TresMesh :position="[0, 2, 0]" cast-shadow>
+      <TresSphereGeometry />
+      <TresMeshToonMaterial color="#E8751A" />
+    </TresMesh>
     <TresAxesHelper />
+    <TresGridHelper :args="[10, 10, 0x444444, 'teal']" />
   </TresCanvas>
 </template>
--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,25 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueJsx from '@vitejs/plugin-vue-jsx';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
-    vueJsx(),
+    vue({
+      template: {
+        compilerOptions: {
+          isCustomElement: (tag) =>
+            (tag.startsWith('Tres') && tag !== 'TresCanvas') || tag === 'primitive'
+        }
+      }
+    }),
+    vueJsx()
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
-})
+});

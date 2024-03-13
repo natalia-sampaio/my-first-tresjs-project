0e3ba7dd39e5b16ade23fab5db4002bf031169fd
Natalia Andrade <natalia.srsa@proton.me>
2024-03-12 23:55:27 -0300

    exploring materials

--- src/components/TheExperience.vue
+++ src/components/TheExperience.vue
@@ -1,19 +1,24 @@
 <script setup lang="ts">
 import { TresCanvas } from '@tresjs/core';
-import { SRGBColorSpace } from 'three';
+import { DoubleSide, SRGBColorSpace } from 'three';
 import { OrbitControls } from '@tresjs/cientos';
 
 const positionsArray = new Float32Array([0, 0, 0, 0, 1, 0, 1, 0, 0]);
 </script>
 
 <template>
-  <TresCanvas clear-color="#2c3846" :output-color-space="SRGBColorSpace">
+  <TresCanvas clear-color="#2c3846" :output-color-space="SRGBColorSpace" alpha>
     <TresPerspectiveCamera />
     <OrbitControls />
     <TresMesh>
-      <TresBufferGeometry :position="[positionsArray, 3]" />
-      <TresMeshBasicMaterial color="#ff0000" wireframe />
+      <TresTorusKnotGeometry :args="[1, 0.4, 32, 32]" />
+      <TresMeshPhysicalMaterial color="silver" :emissive-intensity="0.5" />
     </TresMesh>
-    <TresAxesHelper :args="[0.5]" />
+    <TresMesh :position="[4, 0, 0]">
+      <TresTorusGeometry :args="[1, 0.4, 100, 1000]" />
+      <TresMeshToonMaterial color="pink" />
+    </TresMesh>
+    <TresAmbientLight :args="[0xffffff, 1]" />
+    <TresDirectionalLight :args="[0xffffff, 2]" :position="[0, 4, 0]" />
   </TresCanvas>
 </template>
